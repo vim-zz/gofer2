cd286c9637ab82bedaa0b2689928828e254832c0
ofer affias <ofer.affias@gmail.com>
2025-02-13 18:31:08 +0200

    Gofer icons

--- src/logger.rs
+++ src/logger.rs
@@ -4,7 +4,7 @@ use oslog::OsLogger;
 /// Initializes the logger for the application.
 /// Call this early in main.
 pub fn init_logger() {
-    OsLogger::new("com.1000ants.basicmenubarapp")
+    OsLogger::new("com.1000ants.gofer2")
         .level_filter(LevelFilter::Debug)
         .init()
         .unwrap();
